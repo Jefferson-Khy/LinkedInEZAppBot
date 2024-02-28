39b5d2f467b1d816f093d44b40e8dc0debdaa5d8
jefferson khy <khyjefferson@gmail.com>
2024-02-28 02:33:47 -0600

    moved loginProcedure function into its own js file

---
+++ loginUserInput.js
@@ -0,0 +1,31 @@
+const performLogin = require('./performLogin');
+
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  // Function to request user credentials and attempt login
+  async function loginProcedure(page, delay) {
+    let loginSuccess = false;
+    while (!loginSuccess) {
+      const email = await new Promise((resolve) => {
+        readline.question('Enter your email/username: ', resolve);
+      });
+      const password = await new Promise((resolve) => {
+        readline.question('Enter your password: ', resolve);
+      });
+
+      loginSuccess = await performLogin(page, email, password, delay); // Assuming delay is handled inside performLogin
+      if (loginSuccess) {
+        console.log('Login successful!');
+        // Continue with the rest of your code here after successful login
+      } else {
+        console.log('Login failed, please try again.');
+        // The while loop will continue, asking the user for their credentials again
+      }
+    }
+    readline.close();
+  }
+  
+  module.exports = loginProcedure;
+  
